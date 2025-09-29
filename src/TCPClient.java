d37d11c5af2c2099116616a8c8f2406fb5238fdb
Jax Tobyas <153058960+JaxT05@users.noreply.github.com>
2025-09-29 12:39:35 -0400

    add starter code for upload case and code for rename case

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -4,8 +4,6 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Scanner;
 
- // TODO: Add switch statement for cases Upload and Download
-
 public class TCPClient {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
@@ -16,9 +14,14 @@ public class TCPClient {
         Scanner keyboard = new Scanner(System.in);
         char command;
         do {
-            System.out.println("Enter a command " +
-                    "[L (list), D (delete), R (rename), " +
-                    "E (echo), Q (quit)]");
+            System.out.println("Enter a command: " +
+                    "\nL - list all files" +
+                    "\nX - delete a file" +
+                    "\nR - rename a file" +
+                    "\nU - upload a file" +
+                    "\nD - download a file" +
+                    "\nE - echo a message" +
+                    "\nQ - quit program\n");
             String userInput = keyboard.nextLine();
             command = userInput.toUpperCase().charAt(0);
 
@@ -41,8 +44,8 @@ public class TCPClient {
                     System.out.println(new String(byteArray));
                     break;
                 //delete file
-                case 'D':
-                    System.out.println("Enter the filename that you want to delete:");
+                case 'X':
+                    System.out.println("Enter the filename of what you want to delete:");
                     String clientMessage = keyboard.nextLine();
                     commandBuffer = ByteBuffer.allocate(2);
                     commandBuffer.putChar(command);
@@ -61,7 +64,14 @@ public class TCPClient {
                     replyBuffer.get(byteArray);
                     System.out.println(new String(byteArray));
                     break;
+                //rename file >> may not work, this will have to be tested
                 case 'R':
+                    System.out.println("Enter the filename of what you want to rename:");
+                    String fileName = keyboard.nextLine();
+                    System.out.println("Enter the new name:");
+                    String newFileName = keyboard.nextLine();
+                    //putting everything into a string with a character separator for the server
+                    clientMessage = fileName + "/" + newFileName;
                     commandBuffer = ByteBuffer.allocate(2);
                     commandBuffer.putChar(command);
                     commandBuffer.flip();
@@ -69,6 +79,8 @@ public class TCPClient {
                     channel.connect(new InetSocketAddress(args[0], serverPort));
                     channel.write(commandBuffer);
                     channel.shutdownOutput();
+                    messageBuffer = ByteBuffer.wrap(clientMessage.getBytes());
+                    channel.write(messageBuffer);
                     replyBuffer = ByteBuffer.allocate(1024);
                     bytesRead = channel.read(replyBuffer);
                     channel.close();
@@ -76,6 +88,47 @@ public class TCPClient {
                     byteArray = new byte[bytesRead];
                     replyBuffer.get(byteArray);
                     System.out.println(new String(byteArray));
+                    break;
+                //upload file >> unfinished
+                case 'U':
+                    System.out.println("Enter the filename of what you want to upload:");
+                    fileName = keyboard.nextLine();
+                    //send command
+                    commandBuffer = ByteBuffer.allocate(2);
+                    commandBuffer.putChar(command);
+                    commandBuffer.flip();
+                    channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(args[0], serverPort));
+                    channel.write(commandBuffer);
+                    channel.shutdownOutput();
+                    //send file name
+                    ByteBuffer fileNameBuffer = ByteBuffer.allocate(4);
+                    fileNameBuffer.putInt(fileName.length());
+                    fileNameBuffer.flip();
+                    //TODO: send file content
+                    break;
+                //download file
+                case 'D':
+                    //TODO: write download case
+                    System.out.println("Enter the filename of what you want to rename:");
+                    fileName = keyboard.nextLine();
+                    commandBuffer = ByteBuffer.allocate(2);
+                    commandBuffer.putChar(command);
+                    commandBuffer.flip();
+                    channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(args[0], serverPort));
+                    channel.write(commandBuffer);
+                    channel.shutdownOutput();
+                    messageBuffer = ByteBuffer.wrap(fileName.getBytes());
+                    channel.write(messageBuffer);
+                    //write something that intercepts the file content
+//                    replyBuffer = ByteBuffer.allocate(1024);
+//                    bytesRead = channel.read(replyBuffer);
+//                    channel.close();
+//                    replyBuffer.flip();
+//                    byteArray = new byte[bytesRead];
+//                    replyBuffer.get(byteArray);
+//                    System.out.println(new String(byteArray));
                     break;
                 case 'E':
                     System.out.println("Enter the message:");
@@ -120,6 +173,8 @@ public class TCPClient {
             }
         } while (command != 'Q');
     }
+
+    //methods to clean up switch statement
     public static void wrapCommand (char command, String[] args, int serverPort) throws IOException {
         ByteBuffer commandBuffer = ByteBuffer.allocate(2);
         commandBuffer.putChar(command);
