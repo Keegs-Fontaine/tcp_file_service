6153a67c802fc41fc5ab11cc66b476978485abb3
Jax Tobyas <153058960+JaxT05@users.noreply.github.com>
2025-11-03 19:18:40 -0500

    immediately fix server code

--- src/ClientAcceptance.java
+++ src/ClientAcceptance.java
@@ -1,26 +1,30 @@
 import java.io.IOException;
-import java.net.InetSocketAddress;
+
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ClientAcceptance implements Runnable {
-    static final int PORT = 3000;
+    ServerSocketChannel listenSocket;
+
+    public ClientAcceptance(ServerSocketChannel listenSocket) {
+        this.listenSocket = listenSocket;
+    }
 
     public void run() {
-        ExecutorService es;
-        try (ServerSocketChannel listenSocket = ServerSocketChannel.open()) {
-            es = Executors.newSingleThreadExecutor();
-            listenSocket.bind(new InetSocketAddress(PORT));
 
-            while (true) {
-                SocketChannel serverChannel = listenSocket.accept();
-                System.out.println("Client Connected.");
-                es.submit(new RunServerTask(serverChannel));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        ExecutorService es;
+        es = Executors.newSingleThreadExecutor();
+
+            do {
+                try {
+                    SocketChannel serverChannel = listenSocket.accept();
+                    System.out.println("Client Connected.");
+                    es.submit(new RunServerTask(serverChannel));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } while (true);
     }
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,27 +1,33 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class TCPServer {
-    public static void main(String[] args) {
+    static final int PORT = 3000;
+    public static void main(String[] args) throws IOException {
+
         Scanner sc = new Scanner(System.in);
+        String keyboardInput = "";
+
+        ServerSocketChannel listenSocket = ServerSocketChannel.open();
+        listenSocket.bind(new InetSocketAddress(PORT));
+        System.out.println("Listening on port " + PORT);
 
         ExecutorService es = Executors.newFixedThreadPool(4);
+        es.submit(new ClientAcceptance(listenSocket));
 
-        System.out.println("Enter 'q' to shut down the server.");
-
-        String keyboardInput = sc.nextLine();
-
-        while (!keyboardInput.equals("q")) {
-            es.submit(new ClientAcceptance());
+        do {
+            System.out.println("Enter 'q' to shut down the server.");
+            keyboardInput = sc.nextLine();
         }
-        //elegantly handle this later
-        es.shutdown();
+        while (!keyboardInput.equals("q"));
+
         sc.close();
+        es.shutdown();
+        listenSocket.close();
         System.out.println("Server shut down");
     }
 }
